d70234a14ebbd29c095684fff12ea2e411ebc3a4
rBurgett <ryan@burgettweb.net>
2014-09-24 12:27:10 -0400

    Added default admin user

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,20 +1,27 @@
 if (PyroPages.find().count() === 0) {
     PyroPages.insert({
         'path' : '/',
-        'title' : 'home',
+        'title' : 'Home',
         'pageOrder' : '1',
         'contents' : 'This is the home page contents.'
     });
     PyroPages.insert({
         'path' : 'about',
-        'title' : 'about',
-        'pageOrder' : '1',
+        'title' : 'About',
+        'pageOrder' : '2',
         'contents' : 'This is the about page contents.'
     });
     PyroPages.insert({
         'path' : 'contact',
-        'title' : 'contact',
-        'pageOrder' : '1',
+        'title' : 'Contact',
+        'pageOrder' : '3',
         'contents' : 'This is the contact page contents.'
+    });
+};
+if (Meteor.users.find().count() === 0) {
+    Accounts.createUser({
+        'username' : 'admin',
+        'password' : 'adminpass',
+        'profile' : {'role' : 'administrator'}
     });
 };
